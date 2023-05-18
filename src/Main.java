c074350011ff82e1fee0f610bf22188062948c34
”Denys <syrotiuk.den@me.com>
2023-05-19 00:36:16 +0300

    Реализован Декодинг

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import constants.Constants;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -7,75 +8,63 @@ import java.nio.file.Paths;
 public class Main {
     public static void main(String[] args) {
         // Входящие аргументы
-        String command = "[ENCRYPT]"; //[ENCRYPT, DECRYPT, BRUTE_FORCE]
-        String filePath = "/Users/denyssyrotiuk/Документи на MacMini/Java/IntelliJ IDEA CT/CaesarsCipher/src/files/Hamlet.txt";
+        String command = "[DECRYPT]"; //[ENCRYPT, DECRYPT, BRUTE_FORCE]
+        String filePath = "/Users/denyssyrotiuk/Документи на MacMini/Java/IntelliJ IDEA CT/CaesarsCipher/src/files/Hamlet[ENCRYPT].txt";
         int key = 5;
         ////
 
-        // КОДИРОВАНИЕ
-        if (command.equals("[ENCRYPT]")) {
-            int remainderOfKey = key % Constants.ALPHABET_ROMAN_UPPER_CASE.length;
-            Path inputFile = Path.of(filePath);
-            try {
-                String inputFileToString = Files.readString(inputFile);
-                char[] inputFileToCharArray = inputFileToString.toCharArray();
 
-                for (int i = 0; i < inputFileToCharArray.length; i++) {
-                    if (Character.isLetter(inputFileToCharArray[i])) {
-                        if (Character.isUpperCase(inputFileToCharArray[i])) {
+        if (command.equals("[DECRYPT]")) {  // для дешифровки
+            key = Constants.ALPHABET_ROMAN_UPPER_CASE.length - key;
+        }
 
-                            for (int j = 0; j < Constants.ALPHABET_ROMAN_UPPER_CASE.length; j++) {
-                                if (inputFileToCharArray[i] == Constants.ALPHABET_ROMAN_UPPER_CASE[j]) {
-                                    if ((j + remainderOfKey) < Constants.ALPHABET_ROMAN_UPPER_CASE.length) {
-                                        inputFileToCharArray[i] = Constants.ALPHABET_ROMAN_UPPER_CASE[j + remainderOfKey];
-                                        break;
-                                    } else {
-                                        int remainderNumber = j + remainderOfKey - Constants.ALPHABET_ROMAN_UPPER_CASE.length;
-                                        inputFileToCharArray[i] = Constants.ALPHABET_ROMAN_UPPER_CASE[remainderNumber];
-                                        break;
-                                    }
+        int remainderOfKey = key % Constants.ALPHABET_ROMAN_UPPER_CASE.length;
+        Path inputFile = Path.of(filePath);
+        try {
+            String inputFileToString = Files.readString(inputFile);
+            char[] inputFileToCharArray = inputFileToString.toCharArray();
+
+            for (int i = 0; i < inputFileToCharArray.length; i++) {
+                if (Character.isLetter(inputFileToCharArray[i])) {
+                    if (Character.isUpperCase(inputFileToCharArray[i])) {
+
+                        for (int j = 0; j < Constants.ALPHABET_ROMAN_UPPER_CASE.length; j++) {
+                            if (inputFileToCharArray[i] == Constants.ALPHABET_ROMAN_UPPER_CASE[j]) {
+                                if ((j + remainderOfKey) < Constants.ALPHABET_ROMAN_UPPER_CASE.length) {
+                                    inputFileToCharArray[i] = Constants.ALPHABET_ROMAN_UPPER_CASE[j + remainderOfKey];
+                                    break;
+                                } else {
+                                    int remainderNumber = j + remainderOfKey - Constants.ALPHABET_ROMAN_UPPER_CASE.length;
+                                    inputFileToCharArray[i] = Constants.ALPHABET_ROMAN_UPPER_CASE[remainderNumber];
+                                    break;
                                 }
                             }
-                        } else if (Character.isLowerCase(inputFileToCharArray[i])) {
-                            for (int j = 0; j < Constants.ALPHABET_ROMAN_LOWER_CASE.length; j++) {
-                                if (inputFileToCharArray[i] == Constants.ALPHABET_ROMAN_LOWER_CASE[j]) {
-                                    if ((j + remainderOfKey) < Constants.ALPHABET_ROMAN_LOWER_CASE.length) {
-                                        inputFileToCharArray[i] = Constants.ALPHABET_ROMAN_LOWER_CASE[j + remainderOfKey];
-                                        break;
-                                    } else {
-                                        int remainderNumber = j + remainderOfKey - Constants.ALPHABET_ROMAN_LOWER_CASE.length;
-                                        inputFileToCharArray[i] = Constants.ALPHABET_ROMAN_LOWER_CASE[remainderNumber];
-                                        break;
-                                    }
+                        }
+                    } else if (Character.isLowerCase(inputFileToCharArray[i])) {
+                        for (int j = 0; j < Constants.ALPHABET_ROMAN_LOWER_CASE.length; j++) {
+                            if (inputFileToCharArray[i] == Constants.ALPHABET_ROMAN_LOWER_CASE[j]) {
+                                if ((j + remainderOfKey) < Constants.ALPHABET_ROMAN_LOWER_CASE.length) {
+                                    inputFileToCharArray[i] = Constants.ALPHABET_ROMAN_LOWER_CASE[j + remainderOfKey];
+                                    break;
+                                } else {
+                                    int remainderNumber = j + remainderOfKey - Constants.ALPHABET_ROMAN_LOWER_CASE.length;
+                                    inputFileToCharArray[i] = Constants.ALPHABET_ROMAN_LOWER_CASE[remainderNumber];
+                                    break;
                                 }
                             }
                         }
                     }
                 }
-                String outputFileToString = new String(inputFileToCharArray);
-                Path outputFile = Paths.get(Encrypt.getNewFileName(filePath, command));
-                if (Files.notExists(outputFile)) {
-                    Files.createFile(outputFile);
-                }
-                Files.writeString(outputFile, outputFileToString);
-            } catch (IOException ioe) {
-                ioe.printStackTrace();
             }
+            String outputFileToString = new String(inputFileToCharArray);
+            Path outputFile = Paths.get(Encrypt.getNewFileName(filePath, command));
+            if (Files.notExists(outputFile)) {
+                Files.createFile(outputFile);
+            }
+            Files.writeString(outputFile, outputFileToString);
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
         }
-
-
-        // ДЕКОДИНГ
-        if (command.equals("[DECRYPT]")) {
-
-        }
-
-
-
-
-
-
-
-
 
     }
 }
