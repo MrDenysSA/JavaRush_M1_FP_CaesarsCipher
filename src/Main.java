0130100f3186e6bae209e49eac377940a5bf215a
”Denys <syrotiuk.den@me.com>
2023-05-19 20:22:44 +0300

    работа с аргументами

--- src/Main.java
+++ src/Main.java
@@ -9,9 +9,42 @@ public class Main {
     public static void main(String[] args) {
         // Входящие аргументы
         String command = "[DECRYPT]"; //[ENCRYPT, DECRYPT, BRUTE_FORCE]
-        String filePath = "/Users/denyssyrotiuk/Документи на MacMini/Java/IntelliJ IDEA CT/CaesarsCipher/src/files/Hamlet[ENCRYPT].txt";
-        int key = 5;
+        String filePath = "/Users/denyssyrotiuk/Документи на MacMini/Java/IntelliJ IDEA CT/CaesarsCipher/src/files/Hamlet.txt";
+        int key = 0;
         ////
+
+        for (String arg: args) {
+            // command
+            if (args[0].equals("ENCRYPT")){
+                command = "[ENCRYPT]";
+            } else if (args[0].equals("DECRYPT")) {
+                command = "[DECRYPT]";
+            } else if (args[0].equals("BRUTE_FORCE")) {
+                command = "[BRUTE_FORCE]";
+            } else {
+                System.out.println("Command error. Please enter the command: ENCRYPT or DECRYPT or BRUTE_FORCE");
+                break;
+            }
+
+            boolean x = Files.exists(Path.of(args[1]));
+            boolean y = Files.isRegularFile(Path.of(args[1]));
+            boolean t = Files.isDirectory(Path.of(args[1]));
+            //filePath
+            if (args[1]==null) {
+                System.out.println("Command error. Please enter the command: ENCRYPT or DECRYPT or BRUTE_FORCE");
+            }
+                filePath = args[1];
+
+
+
+
+            //key
+            if (args[0].equals("ENCRYPT")|| args[0].equals("DECRYPT")){
+                key = Integer.parseInt(args[2]);
+            }
+
+
+        }
 
 
         if (command.equals("[DECRYPT]")) {  // для дешифровки
@@ -68,4 +101,3 @@ public class Main {
 
     }
 }
-
